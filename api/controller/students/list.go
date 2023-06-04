4e3f8feedfcf6ec5c9cafde7396c1a5d467482ad
Luiz Henrique <luizhjramos@outlook.com>
2023-06-04 15:36:44 -0300

    feat: add list student controller

--- api/controller/students/list.go
+++ api/controller/students/list.go
@@ -1,1 +1,13 @@
-package controller
+package students
+
+import (
+	"net/http"
+
+	"github.com/MogLuiz/students-api/entity"
+	"github.com/gin-gonic/gin"
+)
+
+func List(c *gin.Context) {
+	c.JSON(http.StatusOK, entity.Students)
+	c.Done()
+}
