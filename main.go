dd5bda8c2e433ccc58dd9fd3a603ce9203be4346
Luiz Henrique <luizhjramos@outlook.com>
2023-06-04 12:32:02 -0300

    feat: add show student route

--- main.go
+++ main.go
@@ -126,11 +126,38 @@ func deleteStudent(c *gin.Context) {
 	c.Done()
 }
 
+func showStudent(c *gin.Context) {
+	id, err := strconv.Atoi(c.Params.ByName("id"))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": "Invalid id",
+		})
+		c.Done()
+		return
+	}
+
+	for _, s := range students {
+		if s.ID == id {
+			c.JSON(http.StatusOK, gin.H{
+				"data": s,
+			})
+			c.Done()
+			return
+		}
+	}
+
+	c.JSON(http.StatusNotFound, gin.H{
+		"message": "Student not found",
+	})
+	c.Done()
+}
+
 func getRoutes(c *gin.Engine) *gin.Engine {
 	c.GET("/heart", routeHeart)
 
 	groupStudents := c.Group("/students")
 	groupStudents.GET("/", listStudents)
+	groupStudents.GET("/:id", showStudent)
 	groupStudents.POST("/", createStudent)
 	groupStudents.PUT("/:id", updateStudent)
 	groupStudents.DELETE("/:id", deleteStudent)
