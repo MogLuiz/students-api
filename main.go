5be14115038702a8487c5918f8d7aa53c1d655de
Luiz Henrique <luizhjramos@outlook.com>
2023-06-04 00:40:30 -0300

    feat: add routes structure

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	fmt.Println("Starting server...")
+	r := gin.Default()
+	r = getRoutes(r)
+	r.Run(":8080")
+}
+
+func routeHeart(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Ok",
+	})
+}
+
+func getRoutes(c *gin.Engine) *gin.Engine {
+	c.GET("/heart", routeHeart)
+
+	return c
 }
